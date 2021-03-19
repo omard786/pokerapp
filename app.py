d3e9abd994c475502e586f7ac0f8eb118146c553
omardhin <omardhin@gmail.com>
2021-03-19 16:24:10 +0000

    pulling in 2 different sets of data

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask,request, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from application import app
+from application import db
+from application.models import poker_player, tournement, ranking
+from flask import render_template
+
+
+#secretkey for forms
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
+class players_form(FlaskForm):
+    first_name = StringField('firstname')
+    last_name = StringField('lastname')
+    age = StringField('age')
+    city = StringField('city')
+    submit = SubmitField('add_player')
+
+class tournements_form(FlaskForm):
+    tournement_id= StringField('tournement_id')
+    location = StringField('location')
+    time_starting = StringField('time starting')
+    player_id= StringField('player_id')
+    submit = SubmitField('add_tournement')
+
+class ranking_form(FlaskForm):
+    ranking_id = StringField('Ranking_id')
+    player_id= StringField('player_id')
+    tournement_id = StringField('tournement_id')
+    submit = SubmitField('add_your_rank')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/player', methods=['GET', 'POST'])
+def player():
+    # form = players_form()
+    # if request.method=='POST':
+    #     a= request.form['firstname']
+    #     b= request.form['lastname']
+    #     c= request.form['age']
+    #     d= request.form['city']
+    #     newplayer= poker_app(first_name=a, last_name=b, age=c, city=d)
+    #     db.session.add(newplayer)
+    #     db.session.commit()
+        return render_template('players.html')
+        # form=form)
+
+
+@app.route('/tournement_enter', methods=['GET', 'POST'])
+def tournement_enter():
+    form = tournements_form()
+    if  request.method=='POST':
+        a= request.form['tournement_id']
+        b= request.form['location']
+        c= request.form['timestarting']
+        d= request.form['player_id']
+        newtournement= poker_app(tournement_id=a, location=b, time_starting=c, player_id=d)
+        db.session.add(newtournement)
+        db.session.commit()
+    return render_template('tournement.html', form=form)
+
+@app.route('/ranking', methods=['GET', 'POST'])
+def ranking():
+    form = ranking_form()
+    if request.method=='POST':
+        a= request.form['Ranking_id']
+        b= request.form['player_id']
+        c= request.form['tournement_id']
+        d= request.form['prize_won']
+        newranking= poker_app(ranking_id=a, player_id=b, tournement_id=c, prize_won=d)
+        db.session.add(newranking)
+        db.session.commit()
+    return render_template('ranking.html', form=form)
+
+@app.route('/allplayers')
+def allplayers():
+    allplayers=poker_player.query.all()
+    name_string= ""
+    for item in allplayers:
+        name_string+="<br>"+item.first_name
+    return name_string
+
+@app.route('/alltournements')
+def alltournements():
+    alltournements=tournement.query.all()
+    tourn_time=""
+    for item in alltournements:
+        tourn_time+="<br>"+ str(item.time_starting)+" "+item.location
+    return tourn_time 
+
+@app.route('/rankings')
+def rankings():
+    rankings=ranking.query.all()
+    place=""
+    for item in ranking:
+        place+="<br>"+ str(item.position)
+        return place
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ create.py
@@ -0,0 +1,46 @@
+from application import db
+from application.models import poker_player, tournement, ranking
+
+db.create_all()
+
+Omar= poker_player(first_name='Omar', last_name= 'Dhin', age= '23', city='bradford')
+Jack= poker_player(first_name='Jack', last_name= 'Dhin', age= '27', city='bristol')
+Oliver= poker_player(first_name='Oliver', last_name= 'Oakley', age= '22', city='kent')
+Michael= poker_player(first_name='Michael', last_name= 'Webster', age= '26', city='leeds')
+Kelvin= poker_player(first_name='Kelvin', last_name= 'Bastow', age= '24', city='tamworth')
+Hamza= poker_player(first_name='Hamza', last_name= 'Dhin', age='20', city= 'leeds')
+Usama= poker_player(first_name='Usama', last_name ='Nasir', age='23', city='bradford')
+
+db.session.add(Omar)
+db.session.add(Jack)
+db.session.add(Oliver)
+db.session.add(Michael)
+db.session.add(Kelvin)
+db.session.add(Hamza)
+db.session.add(Usama)
+db.session.commit()
+
+beginner_tourn= tournement(location= 'meeting room 1', time_starting= '14:00')
+intermediate_tourn= tournement(location= 'meeting room 2', time_starting= '16:00')
+professional_tourn= tournement(location= 'roof', time_starting= '18:00')
+
+db.session.add(beginner_tourn)
+db.session.add(intermediate_tourn)
+db.session.add(professional_tourn)
+db.session.commit()
+
+one=ranking(position='1')
+two=ranking(position='2')
+three=ranking(position='3')
+four=ranking(position='4')
+five=ranking(position='5')
+six=ranking(position='6')
+seven=ranking(position='7')
+
+db.session.add(one)
+db.session.add(two)
+db.session.add(three)
+db.session.add(four)
+db.session.add(five)
+db.session.add(six)
+db.session.add(seven)
